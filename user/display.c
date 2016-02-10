817b3e7bdab2e2364c50c6b947ed1dc158c2fbe7
Henri Roosen <henriroosen@gmail.com>
2016-02-10 22:04:40 +0100

    display: add test output

--- user/display.c
+++ user/display.c
@@ -8,6 +8,7 @@
 
 #include "driver/i2c_oled.h"
 #include "beerctrl.h"
+#include "display.h"
 
 
 void ICACHE_FLASH_ATTR DISPLAY_BeerCtrlEvent(int event, int state)
@@ -30,7 +31,7 @@ void ICACHE_FLASH_ATTR DISPLAY_BeerCtrlEvent(int event, int state)
 		}
 		break;
 	case BCTRL_EVENT_KP:
-		os_sprintf(buf, "%d", state);
+		os_sprintf(buf, "%02d", state);
 		OLED_Print(14, 7, "KP:", 1);
 		OLED_Print(17, 7, buf, 1);
 		break;
@@ -51,16 +52,24 @@ void ICACHE_FLASH_ATTR DISPLAY_BeerCtrlEvent(int event, int state)
 	}
 
 	if (event >= BCTRL_EVENT_ERR_BASE && event < BCTRL_EVENT_ERR_BASE + BCTRL_EVENT_MAX_INDEX) {
-		os_sprintf(buf, "%d", state);
+		os_sprintf(buf, "%05d", state);
 		OLED_Print(0, 3, "err:", 1);
 		OLED_Print(5, 3, buf, 1);
 	} else if (event >= BCTRL_EVENT_OUT_BASE && event < BCTRL_EVENT_OUT_BASE + BCTRL_EVENT_MAX_INDEX) {
-		os_sprintf(buf, "%d", state);
+		os_sprintf(buf, "%05d", state);
 		OLED_Print(0, 4, "out:", 1);
 		OLED_Print(5, 4, buf, 1);
 	} else if (event >= BCTRL_EVENT_READING_BASE && event < BCTRL_EVENT_READING_BASE + BCTRL_EVENT_MAX_INDEX) {
 		temp_to_string(state, buf, sizeof(buf));
 		OLED_Print(0, 0, buf, 2);
-		os_printf("Temp (%d): %s Celsius\r\n", event - BCTRL_EVENT_READING_BASE, buf);
+		os_printf("Temp (%02d): %s Celsius\r\n", event - BCTRL_EVENT_READING_BASE, buf);
+	} else if (event == DISPLAY_EVENT_TEST) {
+		os_sprintf(buf, "%04d", state);
+		OLED_Print(0, 1, "tst1:", 1);
+		OLED_Print(5, 1, buf, 1);
+	} else if (event == DISPLAY_EVENT_TEST_2) {
+		os_sprintf(buf, "%04d", state);
+		OLED_Print(0, 2, "tst2:", 1);
+		OLED_Print(5, 2, buf, 1);
 	}
 }
--- user/display.h
+++ user/display.h
@@ -8,6 +8,9 @@
 #ifndef USER_DISPLAY_H_
 #define USER_DISPLAY_H_
 
+#define DISPLAY_EVENT_TEST    255
+#define DISPLAY_EVENT_TEST_2  254
+
 void DISPLAY_BeerCtrlEvent(int event, int state);
 
 #endif /* USER_DISPLAY_H_ */
