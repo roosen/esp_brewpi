bf2700d382cc215755ef524b9a3cd1962f6d3609
Henri Roosen <henriroosen@gmail.com>
2016-05-01 07:41:13 +0200

    Add virtual sensor for debugging

--- user/user_main.c
+++ user/user_main.c
@@ -10,14 +10,19 @@
 #include "driver/i2c_oled.h"
 #include "beerctrl.h"
 #include "display.h"
+#include "rotary.h"
 
 
 #define CONFIG_MQTT
+//#define CONFIG_VIRT_SENSOR
 
 #define DELAY 1000 /* milliseconds */
 #define DS18B20_SCAN_CNT 60
 
 static os_timer_t ds18b20_timer;
+#ifdef CONFIG_VIRT_SENSOR
+static int16_t virt_temp = 15 << 4;
+#endif
 
 
 static void ICACHE_FLASH_ATTR timer_cb(void *arg)
@@ -38,7 +43,12 @@ static void ICACHE_FLASH_ATTR timer_cb(void *arg)
 			break;
 		case 2:
 			ds18b20_read_all();
+#ifdef CONFIG_VIRT_SENSOR
+			if (1) {
+				temp = virt_temp;
+#else
 			if (!ds18b20_get_temp(0, &temp)) {
+#endif
 				BCTRL_ReportNewReading(0, temp);
 				if (cnt >= DS18B20_SCAN_CNT) {
 					char buf[16];
@@ -47,9 +57,9 @@ static void ICACHE_FLASH_ATTR timer_cb(void *arg)
 					temp_to_string(temp, buf, sizeof(buf));
 					os_sprintf(str_url, "api_key=%s&field1=%s", KEY_THINGSPEAK, buf);
 					http_post("http://api.thingspeak.com/update", str_url, NULL);
+					os_printf("-sensor_temp: %s\n", buf);
 				}
 			}
-
 			if (cnt >= DS18B20_SCAN_CNT) {
 				cnt = 0;
 				state = 0;
@@ -71,11 +81,24 @@ static void ICACHE_FLASH_ATTR uart_init(void)
 	uart_div_modify(0, UART_CLK_FREQ / 115200);
 }
 
+#ifdef CONFIG_VIRT_SENSOR
+static void ICACHE_FLASH_ATTR onRotary(int dir)
+{
+	if (dir > 0)
+		virt_temp++;
+	else
+		virt_temp--;
+}
+#endif
+
 void ICACHE_FLASH_ATTR user_init(void)
 {
 	uart_init();
 	i2c_init();
 	OLED_Init();
+#ifdef CONFIG_VIRT_SENSOR
+	ROTARY_init(NULL, onRotary);
+#endif
 
 	BCTRL_Init(DISPLAY_BeerCtrlEvent);
 
@@ -89,5 +112,5 @@ void ICACHE_FLASH_ATTR user_init(void)
 	MQTT_Start();
 #endif /* CONFIG_MQTT */
 
-	os_printf("System started ...\n");
+	os_printf("System started ...%u\n", system_get_chip_id());
 }
