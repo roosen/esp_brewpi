a9d8f4265b769fa0b8cc67b80e23fd88eeccde17
Henri Roosen <henriroosen@gmail.com>
2016-11-15 03:48:34 +0100

    implement heater on GPIO2

--- user/heater.c
+++ user/heater.c
@@ -12,14 +12,21 @@
 
 #include "heater.h"
 
+#define HEATER_MUX PERIPHS_IO_MUX_GPIO2_U
+#define HEATER_FUNC FUNC_GPIO2
+#define HEATER_PIN 2
+
 
 void ICACHE_FLASH_ATTR HEATER_init(void)
 {
+	PIN_FUNC_SELECT(HEATER_MUX, HEATER_FUNC);
 }
 
 void ICACHE_FLASH_ATTR HEATER_setState(int state)
 {
 	if (state == HEATER_STATE_HEATING) {
+		gpio_output_set(0, (1 << HEATER_PIN), (1 << HEATER_PIN), 0);
 	} else {
+		gpio_output_set((1 << HEATER_PIN), 0, (1 << HEATER_PIN), 0);
 	}
 }
